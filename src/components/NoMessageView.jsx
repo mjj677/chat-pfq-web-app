7a96c69ab2dfe1492105b5ed1725fe2915d087cc
Jake Slattery <alexander.slattery@googlemail.com>
2024-07-10 12:20:05 +0100

    added dynamic no message img

--- src/components/NoMessageView.jsx
+++ src/components/NoMessageView.jsx
@@ -1,13 +1,17 @@
 import logo from "/logo.png"
+import darkLogo from "/full-dark-mode.png"
+import { useTheme } from "../ThemeContext";
 
 export const NoMessageView = () => {
+  const { darkMode } = useTheme();
+
 
   return(
     <div className="no-message-container">
         <div className="no-message-box">
 
         <h1 className="no-message-text">Welcome to</h1>
-        <img src={logo} style={{width: '300px'}}/>
+        <img src={darkMode ? darkLogo : logo} style={{width: '300px'}}/>
         <h1 className="no-message-text">Please click a message to view messages</h1>
         </div>
     </div>
